9686cb72f92c8aea459423d57661bfd799d51a62
Muhammadshariq007 <mohdshariqusman@gmail.com>
2025-05-31 00:52:11 +0500

    updateCode

--- resources/views/frontend/thankyou.blade.php
+++ resources/views/frontend/thankyou.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend/layout')
 @section('container')
 <style>
-    .backtowebsite {
+.backtowebsite {
     background: #00742D;
     width: fit-content;
     padding: 10px 30px;
@@ -11,8 +11,9 @@
     margin: auto;
     margin-top: 20px;
 }
-.backtowebsite:hover{
-    color:#fff;
+
+.backtowebsite:hover {
+    color: #fff;
 }
 </style>
 
@@ -20,7 +21,7 @@
     <div class="innerform">
         <h1 class="text-center fs-3 fw-bold">Electronic Voting System</h1>
         <img src="{{asset('frontAsset/img/tick-circle.png')}}" width="70" class="d-block m-auto mt-2" />
-        <p class="m-0 text-center pt-2">Your Vote have been submitted successfully</p>
+        <p class="m-0 text-center pt-2">Thank you! Your vote has been recorded successfully and securely.</p>
         <a href="{{url('/')}}" class="backtowebsite">Back to Website</a>
     </div>
 </div>
